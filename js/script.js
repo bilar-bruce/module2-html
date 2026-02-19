a8bbec40bf62fc04f46d2a13daa9bcfbf31d2766
bilar-bruce <bilar.bruce@gmail.com>
2026-02-19 12:45:39 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,12 @@ event.preventDefault();
 const themeToggle = document.getElementById("themeToggle");
 themeToggle.addEventListener("click",function(){
 document.body.classList.toggle("dark-mode");
+
+if (document.body.classList.contains("dark-mode")){
+    themeToggle.textContent = "Toggle Light Mode";
+}else{
+    themeToggle.textContent = "Toggle Dark Mode";
+}
 });
 
 //toggle skills
